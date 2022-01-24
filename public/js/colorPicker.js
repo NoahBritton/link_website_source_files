4df7c5d797b6e3b67a093bd22ad77f25dd09e848
NoahBritton <noahseslar@gmail.com>
2022-01-23 22:35:17 -0500

    final changes

---
+++ public/js/colorPicker.js
@@ -0,0 +1,28 @@
+function changeTextColor() {
+  document.getElementById("one").style.color = document.getElementById("textColor").value;
+  document.getElementById("two").style.color = document.getElementById("textColor").value;
+  document.getElementById("three").style.color = document.getElementById("textColor").value;
+  document.getElementById("four").style.color = document.getElementById("textColor").value;
+  document.getElementById("five").style.color = document.getElementById("textColor").value;
+  document.getElementById("six").style.color = document.getElementById("textColor").value;
+  document.getElementById("seven").style.color = document.getElementById("textColor").value;
+  document.getElementById("eight").style.color = document.getElementById("textColor").value;
+  document.getElementById("nine").style.color = document.getElementById("textColor").value;
+  document.getElementById("ten").style.color = document.getElementById("textColor").value;
+  document.getElementById("eleven").style.color = document.getElementById("textColor").value;
+  document.getElementById("twelve").style.color = document.getElementById("textColor").value;
+}
+function changeFaceColor() {
+  document.getElementById("one").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("two").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("three").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("four").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("five").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("six").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("seven").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("eight").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("nine").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("ten").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("eleven").style.backgroundColor = document.getElementById("faceColor").value;
+  document.getElementById("twelve").style.backgroundColor = document.getElementById("faceColor").value;
+}
--- src/app.js
+++ src/app.js
@@ -11,17 +11,16 @@ const dir = path.join(__dirname, '../public')
 app.use(express.static(dir))
 
 // App pages
-
 app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"))
-}) 
+})
 
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/about.html"))
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/game.html"))
+app.get('/cool', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/secret.html"))
 })
 
 const port = process.env.PORT || 3000
