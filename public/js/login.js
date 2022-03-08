40213da06541d1e1e88868e0fb80b51cfc608494
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-08 00:07:46 -0500

    login + create account with api

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,41 @@
 const protocol = window.location.protocol
 const host = window.location.host
 
-const loginForm = document.querySelector('#loginForm')
-const emailInput = document.querySelector('#email')
-const passwordInput = document.querySelector('#password')
+const loginForm = document.querySelector("#loginForm")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
 
-loginForm.addEventListener('submit', async (e) => {
+loginForm.addEventListener("submit", async(e) => {
     e.preventDefault()
 
-    const email = emailInput.value;
-    const password = passwordInput.value;
+    const email = emailInput.value
+    const password = passwordInput.value
+    const data = { email, password }
 
-    const url = `${protocol}//${host}/?email=${email}&password=${password}`
+    const url = "http://localhost:3002/users/login"
+    //const url = 'https://<<your-api-server-domain>>/users/login'
 
-    let response = await fetch(url)
-    console.log(response.status)
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }
 
-    if (response.status === 401) {
-        const message = document.querySelector('#message')
+    let response = await fetch(url, options)
+
+    if (response.status === 400) {
+        const message = document.querySelector("#message")
         message.textContent = "Invalid email or password."
     } 
-    else if (response.status === 200){
+    else if (response.status === 200) {
         const data = await response.json()
         
-        const newUrl = `${protocol}//${host}/home?token=${data.token}`
-        window.location.replace(newUrl);
+        localStorage.setItem("token", data.token)
+        alert(data.token)
+
+        const newUrl = `${protocol}//${host}/main`
+        window.location.replace(newUrl)
     }
 })
