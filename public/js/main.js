8a731e72439f6b2db86d063f7ccca66549b016b1
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-14 11:36:51 -0400

    modify acc

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const displayAccountItem = document.querySelector("#dropdownMenuButton2")
 const logOutItem = document.querySelector("#logOut")
 const deleteAccItem = document.querySelector("#deleteAcc")
+const modifyAccountModalSaveButton = document.querySelector("#modifyAccountModalSaveButton")
 const protocol = window.location.protocol
 const host = window.location.host
 
@@ -87,4 +88,44 @@ deleteAccItem.addEventListener("click", async(e) => {
   } else {
       console.log("HTTP-Error: " + response.status)
   }
+})
+
+
+modifyAccountModalSaveButton.addEventListener("click", async(e) => {
+    e.preventDefault()
+
+    const token = localStorage.getItem("token")
+
+    const url = "http://localhost:3001/users/me"
+        //const url = 'https://n0code-web-api-4.herokuapp.com/users/me'
+
+    const nameInput = document.querySelector("#nameInput")
+    const passwordInput = document.querySelector("#passwordInput")
+    const name = nameInput.value
+    const password = passwordInput.value
+    const requestData = {...name && { name }, ...password && { password } }
+    //console.log(requestData)
+
+    const options = {
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+    }
+
+    let response = await fetch(url, options)
+
+    if (response.status === 200) {
+        const contentArea = document.querySelector("#contentArea")
+        contentArea.innerHTML = `Saved successful.`
+    } else {
+        console.log("HTTP-Error: " + response.status)
+    }
+
+    const modal = document.querySelector("#modifyAccountModal")
+    bootstrap.Modal.getInstance(modal).hide()
+
+    const form = document.querySelector("#modifyAccountForm").reset()
 })
