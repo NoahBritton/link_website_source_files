e948efb9357a6d02d0beedc69826697a1baa94e1
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-29 08:34:26 -0400

    display a task v2

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,17 +1,7 @@
+const nextTask = document.querySelector("#nextTask")
+
 const host = window.location.host
 const protocol = window.location.protocol
-const taskID = document.querySelector("#taskID")
-const nextTask = document.querySelector("#nextTask")
-const taskArea = document.querySelector("#taskArea")
-const taskDesc = document.querySelector("#taskDesc")
-const taskCard = document.getElementById("#taskCard")
-const taskTitle = document.querySelector("#taskTitle")
-const container = document.getElementById("#taskArea")
-const checkedBox = document.querySelector("#checkedBox")
-const deleteTask = document.querySelector("#deleteTask")
-const modifyTask = document.querySelector("#modifyTask")
-const uncheckedBox = document.querySelector("#uncheckedBox")
-const checkBoxHolder = document.querySelector("#checkBoxHolder")
 
 const skip = 0
 const limit = 1
@@ -19,7 +9,7 @@ const limit = 1
 async function initialLoad() {
   const token = localStorage.getItem("token")
 
-  const url = 'https://nbritton-api-app.herokuapp.com/tasks?skip=0&limit=1'
+  const url = `https://nbritton-api-app.herokuapp.com/tasks?skip=${skip}&limit=${limit}`
   console.log(url)
   const options = {
       method: "GET",
@@ -32,19 +22,28 @@ async function initialLoad() {
 
   if (response.ok) {
       if (response.status === 200) {
+        const taskID = document.querySelector("#taskID")
+        const taskArea = document.querySelector("#taskArea")
+        const taskDesc = document.querySelector("#taskDesc")
+        const taskCard = document.getElementById("#taskCard")
+        const taskTitle = document.querySelector("#taskTitle")
+        const checkedBox = document.querySelector("#checkedBox")
+        const uncheckedBox = document.querySelector("#uncheckedBox")
+        const checkBoxHolder = document.querySelector("#checkBoxHolder")
+
         const data = await response.json()
         console.log(response)
         taskTitle.innerHTML = `${data.title}`
         taskDesc.innerHTML = `${data.description}`
         taskID.innerHTML = `${data._id}`
-        console.log(response)
+
         if (data.completed === false) {
           checkBoxHolder.appendChild(uncheckedBox)
         } else {
           checkBoxHolder.appendChild(checkedBox)
         }
         const cardClone = document.importNode(taskCard, true)
-        container.appendChild(cardClone)
+        taskArea.appendChild(cardClone)
         skip++
       }
   } else {
