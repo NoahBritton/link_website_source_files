0c470bf146364b6957862ad883991e34dec2742f
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-31 16:55:13 -0400

    create new task framework

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,29 +1,30 @@
 /* Buttons */
 const nextTask = document.querySelector("#nextTask")
 const newTask = document.querySelector("#newTask")
+const createNewTask = newTaskContent.querySelector("#createNewTask")
 
 /* Empty task card */
-  // areas
+// areas
 const cardTemp = document.querySelector("#taskCard")
 const taskCard = cardTemp.content.querySelector(".card")
 const cardContent = taskCard.querySelector(".card-body")
-  // specific content areas
+// specific content areas
 const taskID = cardContent.querySelector("#taskID")
 const taskDesc = cardContent.querySelector("#taskDesc")
 const taskTitle = cardContent.querySelector("#taskTitle")
 const checkBoxHolder = cardContent.querySelector("#checkBoxHolder")
-  // check boxes
+// check boxes
 const checkedBoxTemp = document.querySelector("#checkedBox")
 const uncheckedBoxTemp = document.querySelector("#uncheckedBox")
 const checkedBox = checkedBoxTemp.content.querySelector(".form-check")
 const uncheckedBox = uncheckedBoxTemp.content.querySelector(".form-check")
 
 /* New task card */
-  // areas
+// areas
 const newTaskTemp = document.querySelector("#createTaskCard")
 const newTaskCard = newTaskTemp.content.querySelector("#newTaskArea")
 const newTaskContent = newTaskCard.querySelector(".card-body")
-  // inputs
+// inputs
 const titleInput = newTaskContent.querySelector("#titleInput")
 const descInput = newTaskContent.querySelector("#descInput")
 const checkBox = newTaskContent.querySelector("#flexCheckDefault")
@@ -33,9 +34,8 @@ const protocol = window.location.protocol
 
 var skip = 0
 var creatingTask = false
-const limit = 1
 
-newTask.addEventListener("click", function() {
+newTask.addEventListener("click", function () {
   const newTaskCardClone = document.importNode(newTaskCard, true)
   console.log(newTaskCardClone)
   if (!creatingTask) {
@@ -44,81 +44,95 @@ newTask.addEventListener("click", function() {
   }
 });
 
-// createNewTask.addEventListener('submit', async (e) => {
-//     e.preventDefault()
+createNewTask.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const token = localStorage.getItem("token")
+  const title = emailInput.value;
+  const description = usernameInput.value;
+  const completed = passwordInput.value;
+  let data = { title, description, completed }
 
-//     const email = emailInput.value;
-//     const name = usernameInput.value;
-//     const password = passwordInput.value;
-//     let data = { email, password, name }
+  const url = 'https://nbritton-api-app.herokuapp.com/tasks'
 
-//     //const url = 'http://localhost:3002/users'
-//     const url = 'https://nbritton-api-app.herokuapp.com/users'
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  }
 
-//     const options = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     }
+  let response = await fetch(url, options)
 
-//     let response = await fetch(url, options)
-//     data = await response.json()
+  if (response.ok) {
+    if (response.status === 200) {
+      const data = await response.json()
+      const checkBox = checkBoxHolder.querySelector("div")
+      checkBox.remove();
+      taskTitle.innerHTML = `${data[0].title}`
+      taskDesc.innerHTML = `${data[0].description}`
+      taskID.innerHTML = `${data[0]._id}`
 
-//     if (response.status === 400) {
-//         const message = document.querySelector("#message")
-//         message.textContent = data.message
-//     } 
-//     else if (response.status === 201) {
-//         localStorage.setItem("token", data.token)
-//         alert(data.token)
+      if (data[0].completed === false) {
+        const incomplete = document.importNode(uncheckedBox, true)
+        checkBoxHolder.appendChild(incomplete)
+      } else {
+        const completed = document.importNode(checkedBox, true)
+        checkBoxHolder.appendChild(completed)
+      }
 
-//         const newUrl = `${protocol}//${host}/main`
-//         window.location.replace(newUrl)
-//     }
-// });
+      const cardClone = document.importNode(taskCard, true)
+      console.log(cardClone)
+      newTaskCard.remove()
+      taskArea.prepend(cardClone)
+      skip++
+      creatingTask = false
+    }
+  } else {
+    console.log("HTTP-Error: " + response.status)
+  }
+});
 
 async function initialLoad() {
   const token = localStorage.getItem("token")
+  const url = `https://nbritton-api-app.herokuapp.com/tasks?skip=${num}&limit=${limit}`
 
-  const url = `https://nbritton-api-app.herokuapp.com/tasks?skip=${skip}&limit=${limit}`
   console.log(url)
   const options = {
-      method: "GET",
-      headers: {
-          Authorization: `Bearer ${token}`,
-      },
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   }
-  
+
   let response = await fetch(url, options)
 
 
   if (response.ok) {
-      if (response.status === 200) {
-        const data = await response.json()
-        taskTitle.innerHTML = `${data[0].title}`
-        taskDesc.innerHTML = `${data[0].description}`
-        taskID.innerHTML = `${data[0]._id}`
+    if (response.status === 200) {
+      const data = await response.json()
+      taskTitle.innerHTML = `${data[0].title}`
+      taskDesc.innerHTML = `${data[0].description}`
+      taskID.innerHTML = `${data[0]._id}`
 
-        if (data[0].completed === false) {
-          const incomplete = document.importNode(uncheckedBox, true)
-          checkBoxHolder.appendChild(incomplete)
-        } else {
-          const completed = document.importNode(checkedBox, true)
-          checkBoxHolder.appendChild(completed)
-        }
-        const cardClone = document.importNode(taskCard, true)
-        taskArea.appendChild(cardClone)
-        
-        skip++
+      if (data[0].completed === false) {
+        const incomplete = document.importNode(uncheckedBox, true)
+        checkBoxHolder.appendChild(incomplete)
+      } else {
+        const completed = document.importNode(checkedBox, true)
+        checkBoxHolder.appendChild(completed)
       }
+      const cardClone = document.importNode(taskCard, true)
+      taskArea.appendChild(cardClone)
+
+      skip++
+    }
   } else {
-      console.log("HTTP-Error: " + response.status)
+    console.log("HTTP-Error: " + response.status)
   }
 }
 
-nextTask.addEventListener("click", async(e) => {
+nextTask.addEventListener("click", async (e) => {
   e.preventDefault()
 
   const token = localStorage.getItem("token")
@@ -126,39 +140,39 @@ nextTask.addEventListener("click", async(e) => {
   const url = `https://nbritton-api-app.herokuapp.com/tasks?skip=${skip}&limit=${limit}`
   console.log(url)
   const options = {
-      method: "GET",
-      headers: {
-          Authorization: `Bearer ${token}`,
-      },
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   }
-  
+
   let response = await fetch(url, options)
 
   if (response.ok) {
-      if (response.status === 200) {
-        const data = await response.json()
+    if (response.status === 200) {
+      const data = await response.json()
 
-        const checkBox = checkBoxHolder.querySelector("div")
-        checkBox.remove();
-        taskTitle.innerHTML = `${data[0].title}`
-        taskDesc.innerHTML = `${data[0].description}`
-        taskID.innerHTML = `${data[0]._id}`
+      const checkBox = checkBoxHolder.querySelector("div")
+      checkBox.remove();
+      taskTitle.innerHTML = `${data[0].title}`
+      taskDesc.innerHTML = `${data[0].description}`
+      taskID.innerHTML = `${data[0]._id}`
 
-        if (data[0].completed === false) {
-          const incomplete = document.importNode(uncheckedBox, true)
-          checkBoxHolder.appendChild(incomplete)
-        } else {
-          const completed = document.importNode(checkedBox, true)
-          checkBoxHolder.appendChild(completed)
-        }
-
-        const cardClone = document.importNode(taskCard, true)
-        console.log(cardClone)
-        taskArea.appendChild(cardClone)
-        skip++
+      if (data[0].completed === false) {
+        const incomplete = document.importNode(uncheckedBox, true)
+        checkBoxHolder.appendChild(incomplete)
+      } else {
+        const completed = document.importNode(checkedBox, true)
+        checkBoxHolder.appendChild(completed)
       }
+
+      const cardClone = document.importNode(taskCard, true)
+      console.log(cardClone)
+      taskArea.appendChild(cardClone)
+      skip++
+    }
   } else {
-      console.log("HTTP-Error: " + response.status)
+    console.log("HTTP-Error: " + response.status)
   }
 })
 
