84c4595abb0283b0b7a0ebd95c5ea10961de0f60
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-31 23:54:56 -0400

    create new task framework

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -31,7 +31,7 @@ const checkBox = newTaskContent.querySelector("#flexCheckDefault")
 
 const host = window.location.host
 const protocol = window.location.protocol
-dragElement(taskCard);
+dragElement(taskCard)
 
 var skip = 0
 var creatingTask = false
@@ -46,6 +46,12 @@ newTask.addEventListener("click", function () {
     taskArea.prepend(newTaskCardClone)
   }
 });
+
+function cancelTask() {
+  const elm = document.getElementById("taskArea")
+  elm.remove()
+  creatingTask = false
+}
 
 async function submitNewTask() {
   const token = localStorage.getItem("token")
@@ -181,38 +187,3 @@ nextTask.addEventListener("click", async (e) => {
     }
   }
 })
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
